d6757aa8eb806623acd6e9ee4a7074b1e158fca9
Maarten Nouwen <mnouwen@davinci.nl>
2017-03-28 14:26:23 +0200

    Update StudentModel.php

--- model/StudentModel.php
+++ model/StudentModel.php
@@ -2,7 +2,16 @@
 
 function getStudent($id) 
 {
+	$db = openDatabaseConnection();
 
+	$sql = "SELECT * FROM students WHERE student_id = :id";
+	$query = $db->prepare($sql);
+	$query->execute(array(
+		":id" => $id));
+
+	$db = null;
+
+	return $query->fetch();
 }
 
 function getAllStudents() 
@@ -16,12 +25,32 @@ function getAllStudents()
 	$db = null;
 
 	return $query->fetchAll();
-
 }
 
 function editStudent() 
 {
+	$firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
+	$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
+	$gender = isset($_POST['gender']) ? $_POST['gender'] : null;
+	$id = isset($_POST['id']) ? $_POST['id'] : null;
+	
+	if (strlen($firstname) == 0 || strlen($lastname) == 0 || strlen($gender) == 0) {
+		return false;
+	}
+	
+	$db = openDatabaseConnection();
 
+	$sql = "UPDATE students SET student_firstname = :firstname, student_lastname = :lastname, student_gender = :gender WHERE student_id = :id";
+	$query = $db->prepare($sql);
+	$query->execute(array(
+		':firstname' => $firstname,
+		':lastname' => $lastname,
+		':gender' => $gender,
+		':id' => $id));
+
+	$db = null;
+	
+	return true;
 }
 
 function deleteStudent($id = null) 
@@ -48,7 +77,7 @@ function createStudent()
 	$lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
 	$gender = isset($_POST['gender']) ? $_POST['gender'] : null;
 	
-	if (strlen($firstname) == 0 && strlen($firstname) == 0 && strlen($firstname) == 0) {
+	if (strlen($firstname) == 0 || strlen($lastname) == 0 || strlen($gender) == 0) {
 		return false;
 	}
 	
